355705c6b081614e9f8f45139f8f518a91e42495
RiCHi <60302534+Ricardo-Olarte@users.noreply.github.com>
2023-09-19 15:56:10 -0500

    Funciones sin HTTP

--- Virtualizacion_AWS_Docker/src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+++ Virtualizacion_AWS_Docker/src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -1,14 +1,32 @@
 package co.edu.escuelaing.sparkdockerdemolive;
 
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-
+import static spark.Spark.*;
 public class SparkWebServer {
 
     public static void main(String... args){
         port(getPort());
         get("hello", (req,res) -> "Hello Docker!");
+        get("sin", (req,res) -> sin("45"));
+        get("cos", (req,res) -> cos("45"));
+        get("palindromo", (req,res) -> palindromo("amolapaloma"));
+        get("magnitud", (req,res) -> magnitud("2", "3"));
+
+        /*
+        post("sin", new Handle(){
+            @Override
+            public String handle(String s){
+
+            }
+        });
+
+        post("cos", new Handle(){
+            @Override
+            public String handle(String s){
+                return String.valueOf(Math.cos(Double.parseDouble(s)));
+            }
+        });
+        */
     }
 
     private static int getPort() {
@@ -16,6 +34,32 @@ public class SparkWebServer {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
+    }
+
+
+
+
+
+    private static String sin(String s){
+        return String.valueOf(Math.sin(Double.parseDouble(s)));
+    }
+
+    private static String cos(String s){
+        return String.valueOf(Math.cos(Double.parseDouble(s)));
+    }
+
+    private static String palindromo(String string){
+        String c = "";
+        for(int i=(string.length()-1);i>=0;i--){
+            c += string.charAt(i);
+        }
+        return String.valueOf(string==c);
+    }
+
+    private static String magnitud(String s1, String s2){
+        Double a = Double.parseDouble(s1);
+        Double b = Double.parseDouble(s2);
+        return String.valueOf(Math.sqrt(Math.pow(b,2) + Math.pow(a,2)));
     }
 
 }
